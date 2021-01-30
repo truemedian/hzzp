cfd92f9bed64c41e7247a3a83d3d9d81ce69cbf9
Nameless <truemedian@gmail.com>
2021-01-30 12:09:11 -0600

    expose hzzp.Header

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ pub const parser = @import("parser/parser.zig");
 pub const base = @import("base/base.zig");
 
 pub const Headers = @import("headers.zig").Headers;
+pub const Header = @import("common.zig").Header;
 
 comptime {
     std.testing.refAllDecls(@This());
