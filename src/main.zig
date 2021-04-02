03492e32c1b0e8cac21b251462746935f00bef8a
Benjamin Feng <benjamin.feng@glassdoor.com>
2021-04-02 11:09:42 -0500

    Add StatusCode.Group support

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,7 @@ pub const base = @import("base/base.zig");
 
 pub const Headers = @import("headers.zig").Headers;
 pub const Header = @import("common.zig").Header;
+pub const StatusCode = @import("common.zig").StatusCode;
 
 comptime {
     std.testing.refAllDecls(@This());
