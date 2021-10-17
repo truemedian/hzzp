f73620c0a53470e761ebd4b54fafd1cd9bb852f0
Nameless <truemedian@gmail.com>
2021-10-17 18:08:01 -0500

    better benchmark?

--- test/benchmark.zig
+++ test/benchmark.zig
@@ -6,11 +6,17 @@ fn read_timer() u64 {
         \\rdtsc
         \\shlq $32, %%rdx
         \\orq %%rdx, %%rax
-        : [ret] "={rax}" (-> u64)
+        : [ret] "={rax}" (-> u64),
         :
         : "rax", "rdx"
     );
 }
+
+const tests = .{
+    "response1.http",
+    "response1.http",
+    "response1.http",
+};
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -18,42 +24,45 @@ pub fn main() !void {
 
     const allocator = &gpa.allocator;
 
-    const response_content = try std.fs.cwd().readFileAlloc(allocator, "response.http", 2048);
-    defer allocator.free(response_content);
+    inline for (tests) |name| {
+        const response_content = try std.fs.cwd().readFileAlloc(allocator, name, 2048);
+        defer allocator.free(response_content);
 
-    var lowest: u64 = std.math.maxInt(u64);
-    var highest: u64 = std.math.minInt(u64);
-    var average: f64 = 0.0;
+        var lowest: u64 = std.math.maxInt(u64);
+        var highest: u64 = std.math.minInt(u64);
+        var average: f64 = 0.0;
 
-    var trial: u32 = 0;
-    while (trial < 1000000) : (trial += 1) {
-        var response = std.io.fixedBufferStream(response_content);
-        const reader = response.reader();
+        var trial: u32 = 0;
+        while (trial < 1000000) : (trial += 1) {
+            var response = std.io.fixedBufferStream(response_content);
+            const reader = response.reader();
 
-        const start = read_timer();
+            const start = read_timer();
 
-        var buffer: [256]u8 = undefined;
+            var buffer: [256]u8 = undefined;
 
-        var client = hzzp.base.client.create(&buffer, reader, std.io.null_writer);
+            var client = hzzp.base.client.create(&buffer, reader, std.io.null_writer);
 
-        while (try client.next()) |event| {
-            std.mem.doNotOptimizeAway(event);
-            std.math.doNotOptimizeAway(event);
+            while (try client.next()) |event| {
+                std.mem.doNotOptimizeAway(event);
+                std.math.doNotOptimizeAway(event);
+            }
+
+            const stop = read_timer();
+            const time = stop - start;
+
+            if (time < lowest) lowest = time;
+            if (time > highest) highest = time;
+            average += @intToFloat(f64, time);
         }
 
-        const stop = read_timer();
-        const time = stop - start;
+        average = average / @intToFloat(f64, trial);
 
-        if (time < lowest) lowest = time;
-        if (time > highest) highest = time;
-        average += @intToFloat(f64, time);
+        std.debug.print("Test {s}\n", .{name});
+        std.debug.print("Highest: {d}\n", .{highest});
+        std.debug.print("Lowest: {d}\n", .{lowest});
+        std.debug.print("Average: {d:.3}\n\n", .{average});
     }
-
-    average = average / @intToFloat(f64, trial);
-
-    std.debug.print("Highest: {d}\n", .{highest});
-    std.debug.print("Lowest: {d}\n", .{lowest});
-    std.debug.print("Average: {d:.3}\n", .{average});
 }
 
 // zig run benchmark.zig --pkg-begin hzzp ../src/main.zig --pkg-end
