91ab8e741992e8db30b3ee1cd9e7cd5a072ca294
Nameless <truemedian@gmail.com>
2021-10-18 21:17:24 -0500

    improve benchmark tool

--- test/benchmark.zig
+++ test/benchmark.zig
@@ -14,8 +14,54 @@ fn read_timer() u64 {
 
 const tests = .{
     "response1.http",
-    "response1.http",
-    "response1.http",
+    "response2.http",
+    "response3.http",
+};
+
+const BenchmarkTime = struct {
+    lowest: u64 = std.math.maxInt(u64),
+    highest: u64 = std.math.minInt(u64),
+
+    total: u64 = 0,
+    count: usize = 0,
+    bytes: usize = 0,
+
+    fn add(self: *BenchmarkTime, time: u64, bytes: usize) void {
+        if (time > self.highest) {
+            self.highest = time;
+        }
+
+        if (time < self.lowest) {
+            self.lowest = time;
+        }
+
+        self.total += time;
+        self.count += 1;
+
+        // this shouldn't change, but we take it from the first response
+        self.bytes = bytes;
+    }
+
+    fn average(self: BenchmarkTime) f64 {
+        return @intToFloat(f64, self.total) / @intToFloat(f64, self.count);
+    }
+
+    pub fn format(self: BenchmarkTime, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
+        try writer.print("Lo-Hi {d: >6} -> {d: <6} | Avg {d: <9.3} | {d:.6} B/C | {d} entries", .{
+            self.lowest,
+            self.highest,
+            self.average(),
+            @intToFloat(f64, self.bytes) / self.average(),
+            self.count,
+        });
+    }
+};
+
+const Benchmark = struct {
+    initialization: BenchmarkTime,
+    status_line: BenchmarkTime,
+    headers: BenchmarkTime,
+    payload: BenchmarkTime,
 };
 
 pub fn main() !void {
@@ -28,40 +74,56 @@ pub fn main() !void {
         const response_content = try std.fs.cwd().readFileAlloc(allocator, name, 2048);
         defer allocator.free(response_content);
 
-        var lowest: u64 = std.math.maxInt(u64);
-        var highest: u64 = std.math.minInt(u64);
-        var average: f64 = 0.0;
+        var initialization = BenchmarkTime{};
+        var status_line = BenchmarkTime{};
+        var headers = BenchmarkTime{};
+        var payload = BenchmarkTime{};
+        var other = BenchmarkTime{};
 
         var trial: u32 = 0;
-        while (trial < 1000000) : (trial += 1) {
+        while (trial < 1_000_000) : (trial += 1) {
             var response = std.io.fixedBufferStream(response_content);
             const reader = response.reader();
 
-            const start = read_timer();
+            var start: u64 = undefined;
+            var stop: u64 = undefined;
+
+            var pos_start: usize = undefined;
+            var pos_stop: usize = undefined;
+
+            start = read_timer();
 
             var buffer: [256]u8 = undefined;
-
             var client = hzzp.base.client.create(&buffer, reader, std.io.null_writer);
 
+            stop = read_timer();
+
+            initialization.add(stop - start, 0);
+
+            pos_start = response.pos;
+            start = read_timer();
             while (try client.next()) |event| {
-                std.mem.doNotOptimizeAway(event);
-                std.math.doNotOptimizeAway(event);
+                stop = read_timer();
+                pos_stop = response.pos;
+
+                switch (event) {
+                    .status => status_line.add(stop - start, pos_stop - pos_start),
+                    .header => headers.add(stop - start, pos_stop - pos_start),
+                    .payload => payload.add(stop - start, pos_stop - pos_start),
+                    else => other.add(stop - start, pos_stop - pos_start),
+                }
+
+                pos_start = response.pos;
+                start = read_timer();
             }
-
-            const stop = read_timer();
-            const time = stop - start;
-
-            if (time < lowest) lowest = time;
-            if (time > highest) highest = time;
-            average += @intToFloat(f64, time);
         }
 
-        average = average / @intToFloat(f64, trial);
-
         std.debug.print("Test {s}\n", .{name});
-        std.debug.print("Highest: {d}\n", .{highest});
-        std.debug.print("Lowest: {d}\n", .{lowest});
-        std.debug.print("Average: {d:.3}\n\n", .{average});
+        std.debug.print("Initialization: {d}\n", .{initialization});
+        std.debug.print("Status Line:    {d}\n", .{status_line});
+        std.debug.print("Headers:        {d}\n", .{headers});
+        std.debug.print("Payload:        {d}\n", .{payload});
+        std.debug.print("Other:          {d}\n", .{other});
     }
 }
 
